4e40fb5360a0a378f41e402e92fa0671a1f2ec06
Shembou <92935278+Shembou@users.noreply.github.com>
2023-12-29 11:02:51 +0100

    Update LandingPage hero

--- schemas/collectionTypes/LandingPage.js
+++ schemas/collectionTypes/LandingPage.js
@@ -1,63 +1,83 @@
 export default {
   name: 'LandingPage',
-  title: "Strony docelowe",
+  title: 'Strony lądowania',
   type: 'document',
   icon: () => '📜',
   fields: [
     {
       name: 'name',
       type: 'string',
-      title: 'Name'
+      title: 'Name',
     },
     {
       name: 'slug',
       type: 'slug',
       title: 'Slug',
       options: {
-        source: 'name'
-      }
+        source: 'name',
+      },
     },
     {
-      name: 'hero',
-      type: 'Hero',
-      options: { collapsed: true }
+      name: 'hero_Heading',
+      type: 'markdown',
+      title: 'Heading',
+      fieldset: 'hero',
+    },
+    {
+      name: 'hero_Paragraph',
+      type: 'markdown',
+      title: 'Paragraph',
+      fieldset: 'hero',
+    },
+    {
+      name: 'hero_Img',
+      type: 'image',
+      title: 'Image',
+      fieldset: 'hero',
     },
     {
       name: 'content',
       type: 'array',
       of: [
-        { type: 'Slider'},
-        { type: 'CenteredHeading'},
-        { type: 'WindowsShowcase'},
-        { type: 'TextComponent'},
-        { type: 'CaseStudies' },
-        { type: 'ProsAndConsShowcase'},
-        { type: 'HeadingWithTitleAndImgList'},
-        { type: 'HeadingWithIconDescriptionList'},
-        { type: 'ImageShowcase'},
-        { type: 'HeadingDescriptionWithBlocksList'},
-        { type: 'ImageComponent'},
-        { type: 'ProcessList'},
-        { type: 'quickForm'},
-      ]
+        {type: 'Slider'},
+        {type: 'CenteredHeading'},
+        {type: 'WindowsShowcase'},
+        {type: 'TextComponent'},
+        {type: 'CaseStudies'},
+        {type: 'ProsAndConsShowcase'},
+        {type: 'HeadingWithTitleAndImgList'},
+        {type: 'HeadingWithIconDescriptionList'},
+        {type: 'ImageShowcase'},
+        {type: 'HeadingDescriptionWithBlocksList'},
+        {type: 'ImageComponent'},
+        {type: 'ProcessList'},
+        {type: 'quickForm'},
+      ],
     },
     {
       name: 'seo',
       type: 'seo',
       title: 'SEO',
-      group: 'seo'
-    }
+      group: 'seo',
+    },
+  ],
+  fieldsets: [
+    {
+      name: 'hero',
+      title: 'Hero',
+      options: {collapsible: true, collapsed: true},
+    },
   ],
   preview: {
     select: {
       title: 'name',
-      subtitle: 'slug.current'
-    }
+      subtitle: 'slug.current',
+    },
   },
   groups: [
     {
       name: 'seo',
-      title: 'SEO'
-    }
-  ]
+      title: 'SEO',
+    },
+  ],
 }
